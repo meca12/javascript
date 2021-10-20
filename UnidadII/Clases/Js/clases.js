e82b373c1702048afeb8caa8bea9c29b34d3fb07
MarthaAvila <itit.martha.avil@gmail.com>
2021-10-20 17:47:31 -0500

    Complementando datos de clases

--- UnidadII/Clases/Js/clases.js
+++ UnidadII/Clases/Js/clases.js
@@ -1,9 +1,20 @@
 /*----------------------------
 ------------------------------
-Clases primitivas
+Clases:
+Las clases, son funciones constructoras y siempre las vamos a iniciar con
+Mayúsculas
+Ejemplo: new String()
 =============================*/
 
-//clase string 
+/*----------------------------
+------------------------------
+Clases primitivas: estas comienza con
+new y su nombre de clase, Ejemplo
+    new Boolean()
+=============================*/
+
+/*----------Clases primitivas------------------*/
+//clase string variable de tipo objeto con una clase asignada
 
 let string = new String("Martha Elba coreño avila");
 console.log("string", string);
@@ -21,8 +32,12 @@ console.log("Booleano es", boleano);
 /*----------------------------
 ------------------------------
 Clases Compuestas
+Estas nos pueden ayudar a construir nuestros aarrays
 =============================*/
-
+/*----------------------------
+En una clase de tipo objeto se debe usar parentesis y corchetes
+para indicar propiedades y valores
+/*----------------------------*/
 let perro = new Object({ nombre: "pinky", raza: "perro", edad: 7 });
 
 console.log("objeto", perro);
@@ -34,7 +49,12 @@ console.log("Fruteria array", fruteria);
 /*----------------------------
 ------------------------------
 Clases Creada por el programador
+o clases personalizadas, llamadas función constructoras
 =============================*/
+
+
+
+
 function Persona() {
     //propiedades publicas
     this.nombre;
@@ -46,12 +66,15 @@ yo.nombre = "juan";
 yo.edad = 31;
 
 console.log("imprime los objetos de mi clase", yo);
+let persona = document.querySelector("#objeto");
+let humano = yo.nombre;
+persona.innerHTML = humano;
 
 /*----------------------------
 ------------------------------
-Clases Con parámetros
+Clases Con parámetros o clase dinamica
 =============================*/
-
+//constructor los parentesis llevan parametro
 function Animales(nombre, raza) {
 
     //propiedades publicas con parámetros
